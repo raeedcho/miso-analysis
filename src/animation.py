9a9e2ce5e99d746da7369ca8c5928be94b675679
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2026-03-11 10:52:16 -0400

    feat: add plotting for 2026-02 RCP summit

--- src/animation.py
+++ src/animation.py
@@ -1,20 +1,14 @@
 import matplotlib.pyplot as plt
-from matplotlib.patches import Circle, Rectangle
+from matplotlib.patches import Circle, Rectangle, Ellipse
 from matplotlib import animation
 import numpy as np
+import pandas as pd
 import seaborn as sns
 
-from src.time_slice import get_index_level
+from src.time_slice import get_index_level, slice_by_time
 
 def animate_trial(trial, save_path=None):
-    fig,monitor_ax = plt.subplots(1,1,figsize=(10,10))
-
-    # resampled_trial = (
-    #     trial
-    #     .resample('10ms', level='time')
-    #     .mean()
-    # )
-    resampled_trial = trial
+    fig,[neural_ax, monitor_ax] = plt.subplots(1,2,figsize=(20,10))
 
     center_target=monitor_ax.add_patch(Circle((0,0),10,color='0.3'))
     target_direction = get_index_level(trial,'target direction').iloc[0]
@@ -61,11 +55,11 @@ def animate_trial(trial, save_path=None):
                 color='purple',
             )
 
-        stim_activity = resampled_trial['stim activity'][stimmed_channels]
+        stim_activity = trial['stim activity'][stimmed_channels]
     else:
         stim_indicator = {}
 
-    hand_position =  resampled_trial['hand position']
+    hand_position =  trial['hand position']
     cursor = monitor_ax.add_patch(Circle(hand_position[['x','y']].values[0],2,zorder=100,color='r'))
 
     monitor_ax.set_xlim((-90.,90.))
@@ -73,6 +67,21 @@ def animate_trial(trial, save_path=None):
     monitor_ax.set_xticks([])
     monitor_ax.set_yticks([])
     sns.despine(ax=monitor_ax,left=True,bottom=True)
+
+    neural_pca = trial['neural pca']
+    # Use ellipse to account for non-equal axis limits
+    # x_range = 4, y_range = 2, so width should be 2x height for circular appearance
+    ellipse_width = 0.04
+    ellipse_height = 0.02
+    neural_state = neural_ax.add_patch(Ellipse(neural_pca[[0,1]].values[0], ellipse_width, ellipse_height, zorder=100, color=[0,104/255,55/255]))
+    # Add trail line for showing past 200ms of neural state
+    trail_line, = neural_ax.plot([], [], '-', color=[0,104/255,55/255], alpha=0.4, linewidth=2, zorder=99)
+
+    neural_ax.set_xlim((-2.,2.))
+    neural_ax.set_ylim((-1.,1.))
+    neural_ax.set_xticks([])
+    neural_ax.set_yticks([])
+    sns.despine(ax=neural_ax,left=True,bottom=True)
 
     plt.tight_layout()
 
@@ -105,9 +114,24 @@ def animate_trial(trial, save_path=None):
             else:
                 indicator.set(visible=False)
 
-        return [cursor,center_target] + list(stim_indicator.values()) + list(target_ring.values())
+        # Update trail to show past 200ms
+        trail_start_time = frame_time - pd.Timedelta(milliseconds=200)
+        trail_data = slice_by_time(neural_pca, slice(trail_start_time, frame_time))[[0,1]]
+        if len(trail_data) > 0:
+            trail_line.set_data(trail_data[0].values, trail_data[1].values)
+        else:
+            trail_line.set_data([], [])
 
-    frames = get_index_level(resampled_trial,'time')
+        neural_state.set_center(
+            neural_pca
+            .xs(level='time',key=frame_time)
+            [[0,1]]
+            .values[0]
+        )
+
+        return [cursor,center_target,neural_state,trail_line] + list(stim_indicator.values()) + list(target_ring.values())
+
+    frames = get_index_level(trial,'time')
     frame_interval =  frames.dt.total_seconds().diff().mode()[0]
     anim = animation.FuncAnimation(
         fig,
