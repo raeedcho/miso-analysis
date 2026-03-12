6b0c936ec4a8b0b6591e24b2610975a7e4a8373b
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2026-03-12 17:42:02 -0400

    feat: add a new online stim script to run through channels randomly

---
+++ scripts/online-stim/random_channel_stim.m
@@ -0,0 +1,63 @@
+%% Script to run random channel stim across arrays
+% This script will stimulate across all channels sequentially
+% There will be 10 seconds between each stimulation.
+
+% Stimulation parameters
+num_simul_stim_chans = 1;
+all_chans = [1:96 129:160]; % available channels to loop through sequentially
+active_chans = [2,3,4,5,9,11,12,33,40,52,53,57,61,73,75,77,79,80,88,146,158,160]';
+inactive_chans = setdiff(all_chans,active_chans);
+stim_chans = nchoosek(all_chans,num_simul_stim_chans);
+
+fast_settle_option = 3; % 1=None, 2=Any, 3=Same port, 4= Same front end
+fast_settle_duration = 0.5; %ms
+
+pulse_width=250; % us
+stim_freq=350; % Hz
+stim_duration=150; % ms
+stim_amplitude=25; %uA
+time_between_stim=1.5; %s
+num_stim_repeats=1;
+catch_trials_per_block=1;
+
+% initialize and check xippmex
+addpath(genpath('C:\Program Files (x86)\Ripple\Trellis\Tools\xippmex'))
+xippmex('close')
+status = xippmex;
+if status ~= 1
+    error('unable to initialize xippmex')
+end
+available_stim_chans = xippmex('elec','stim');
+    
+unavailable_stim_chans = setdiff(stim_chans,available_stim_chans);
+if any(unavailable_stim_chans)
+    error('unable to stimulate on requested channels %d',unavailable_stim_chans)
+end
+
+for i = 1:num_stim_repeats
+    stim_chan_order = randperm(length(stim_chans)+catch_trials_per_block);
+    for channum = 1:length(stim_chans)+catch_trials_per_block
+        if stim_chan_order(channum) > length(stim_chans)
+            xippmex('digout', 1:2, [1, 1]); pause(0.001); xippmex('digout', 1:2, [0,0]);
+            fprintf('catch trial - no stim\n')
+            pause(time_between_stim)
+            continue
+        end
+        chosen_chan = stim_chans(stim_chan_order(channum),:);
+        
+        xippmex('stim','enable',0) % disable stim first so step size can be set
+        stim_cmd = xippmexStimCmd(chosen_chan,pulse_width,stim_freq,stim_duration,stim_amplitude);
+        xippmex('stim','enable',1) % re-enable stim
+        xippmex('signal',chosen_chan,'stim',chosen_chan)
+        
+        if fast_settle_option>0
+            xippmex('fastsettle','stim',chosen_chan,fast_settle_option,fast_settle_duration);
+        end
+        
+        % send a digital pulse to mark stim timing, then trigger stim
+        xippmex('digout', 1:2, [1, 1]); pause(0.001); xippmex('digout', 1:2, [0,0]);
+        xippmex('stimseq',stim_cmd)
+        pause(time_between_stim)
+    end
+end
+xippmex('close')
--- scripts/online-stim/sequential_channel_stim.m
+++ scripts/online-stim/sequential_channel_stim.m
@@ -3,7 +3,13 @@
 % There will be 10 seconds between each stimulation.
 
 % Stimulation parameters
-stim_chans = [1:96 129:160]; % available channels to loop through sequentially
+num_simul_stim_chans = 2;
+all_chans = [1:96 129:160]; % available channels to loop through sequentially
+active_chans = [2,3,4,5,9,11,12,33,40,52,53,57,61,73,75,77,79,80,88,146,158,160]';
+inactive_chans = setdiff(all_chans,active_chans);
+% avail_chans = [1 2 3 5 6 9 12 33 34 35 40 43 44 50 52 53 57 59 60 61 63 64 67 69 70 72 73 77 80 81 82 83 84 87 88 90 91 95 133 154 156 158]';
+stim_chans = nchoosek(active_chans,num_simul_stim_chans);
+
 fast_settle_option = 3; % 1=None, 2=Any, 3=Same port, 4= Same front end
 fast_settle_duration = 0.5; %ms
 
@@ -11,9 +17,10 @@ pulse_width=250; % us
 stim_freq=350; % Hz
 stim_duration=150; % ms
 stim_amplitude=25; %uA
-time_between_stim=1; %s
+time_between_stim=3; %s
 
-% initialize and check xippmex
+% initialize and check xippme% initialize and check xippmex
+addpath(genpath('C:\Program Files (x86)\Ripple\Trellis\Tools\xippmex'))
 xippmex('close')
 status = xippmex;
 if status ~= 1
